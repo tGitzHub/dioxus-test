f256f874bc8e53f25ecd0d16df6c99c96603192f
tGitzHub <56638472+tGitzHub@users.noreply.github.com>
2024-02-04 23:32:44 +0100

    wasm doesnt compile, i dont get it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    #[cfg(not(target_arch = "wasm32"))]
+    // launch the dioxus app in a window
+    dioxus_desktop::launch(App);
+
+    #[cfg(target_arch = "wasm32")]
+    // launch the dioxus app in a webview
+    dioxus_web::launch(App);
+}
+
+// define a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+        }
+    })
 }
